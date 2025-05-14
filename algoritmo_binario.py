9d39d9680406d71b4dfa8fa7b7089df78eac88db
frealexandro <jhoner300@gmail.com>
2025-05-13 19:17:43 -0500

    agregamnos el algoritmo binario

---
+++ algoritmo_binario.py
@@ -0,0 +1,27 @@
+
+
+def busqueda_binaria(lista, elemento):
+
+    #!menor y mayor indican en que parte de la lista se buscara
+    menor = 0  
+    mayor =len (lista) -1 
+
+
+    #!minetras no hayas reducido la busqueda a un solo elemento
+    while menor <= mayor :
+        medio = (menor+mayor)//2 #! comprueba el elemento central
+        estimado = lista[medio]
+        if estimado == elemento:#!elemeneto encontrado
+            return medio
+        if estimado > elemento:#! el numero era muy alto
+            mayor = medio-1
+        else : #! el numero era muy bajo
+            menor = medio +1
+
+    return None #!elemento no existe
+
+
+
+mi_lista = [1,3,5,7,9] #! probamos la busqueda
+
+print (busqueda_binaria(mi_lista, 7)) #!llama la funcion 
